629b6eb5c8e188739c57483cb119d5641e4b803f
zxcipo753 <60366443+zxcipo753@users.noreply.github.com>
2021-10-27 12:45:43 -0400

    basic layout

--- app/src/main/java/edu/temple/lab7__001/MainActivity.kt
+++ app/src/main/java/edu/temple/lab7__001/MainActivity.kt
@@ -1,4 +1,18 @@
 package edu.temple.lab7__001
 
-class MainActivity {
-}
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val booksList = BookList()
+        val booksAuthor = resources.getStringArray(R.array.authors)
+        val booksName = resources.getStringArray(R.array.book_titles)
+
+        //Add bookname and author to the booklist array
+        for(i in booksAuthor.indices){
+            booksList.add(Book(booksName[i],booksAuthor[i]))
+        }
