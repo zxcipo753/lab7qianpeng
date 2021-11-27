f99520facb84c4977be78e1a7d298fc2c0d986e8
zxcipo753 <60366443+zxcipo753@users.noreply.github.com>
2021-11-27 05:54:46 -0500

    add audio buttons

---
+++ app/src/main/java/edu/temple/lab7__001/AudioButton.kt
@@ -0,0 +1,93 @@
+package edu.temple.lab7__001
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+
+class AudioButton : Fragment() {
+    lateinit var playButton: Button
+    lateinit var stopButton: Button
+    lateinit var pauseButton: Button
+    lateinit var durationBar: SeekBar
+    lateinit var titleText: TextView
+    lateinit var durationText: TextView
+    var durationInt: Int = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val layout = inflater.inflate(R.layout.fragment_control, container, false)
+
+        playButton = layout.findViewById(R.id.playButton)
+        pauseButton = layout.findViewById(R.id.pauseButton)
+        stopButton = layout.findViewById(R.id.stopButton)
+        titleText = layout.findViewById(R.id.titleText)
+        durationBar = layout.findViewById(R.id.durationBar)
+        durationText = layout.findViewById(R.id.durationText)
+
+        return layout
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bookViewModel = ViewModelProvider(requireActivity()).get(BookViewModel::class.java)
+        playButton.setOnClickListener {
+            val selectedBook = bookViewModel.getBook().value
+
+            if(selectedBook != null){
+                titleText.text = "Start playing >> " + selectedBook.title
+                durationBar.max = selectedBook.duration
+            }
+            (activity as ControlClick).playClick(durationInt)
+        }
+        pauseButton.setOnClickListener {
+            val selectedBook = bookViewModel.getBook().value
+            if (selectedBook != null){
+                durationInt = durationBar.progress
+            }
+            (activity as ControlClick).pauseClick()
+        }
+        stopButton.setOnClickListener {
+            durationInt = 0
+            durationBar.progress = 0
+            (activity as ControlClick).stopClick()
+        }
+        durationBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(bar: SeekBar, progress: Int, p2: Boolean) {
+                val selectedBook = bookViewModel.getBook().value
+                if(selectedBook != null){
+                    durationText.text = progress.toString()
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                (activity as ControlClick).pauseClick()
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                val selectedBook = bookViewModel.getBook().value
+                if(selectedBook != null){
+                    durationInt = durationBar.progress
+                    durationText.text = durationInt.toString()
+                    (activity as ControlClick).playClick(durationInt)
+                }
+            }
+        })
+    }
+
+    interface ControlClick{
+        fun playClick(durationTime: Int)
+        fun pauseClick()
+        fun stopClick()
+        fun seekBarClick()
+    }
+
+}
